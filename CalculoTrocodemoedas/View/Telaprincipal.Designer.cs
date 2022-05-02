9a9923d6206c7e5d14d9e7ad95799175ecc389c9
caioeli <caio.eli0234@gmail.com>
2022-05-02 10:43:28 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoTrocodemoedas/View/Telaprincipal.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace CalculoTrocodemoedas.View
+{
+    partial class Telaprincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelbuy = new System.Windows.Forms.Label();
+            this.labelpaymoney = new System.Windows.Forms.Label();
+            this.labelvaluepay = new System.Windows.Forms.Label();
+            this.labelresultbuy = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.resultcoin1 = new System.Windows.Forms.Label();
+            this.lblcoin1 = new System.Windows.Forms.Label();
+            this.resultcoin05 = new System.Windows.Forms.Label();
+            this.lblcoin05 = new System.Windows.Forms.Label();
+            this.resultcoin025 = new System.Windows.Forms.Label();
+            this.lblcoin025 = new System.Windows.Forms.Label();
+            this.resultcoin010 = new System.Windows.Forms.Label();
+            this.lblcoin010 = new System.Windows.Forms.Label();
+            this.resultcoin005 = new System.Windows.Forms.Label();
+            this.lblcoin005 = new System.Windows.Forms.Label();
+            this.resultcoin001 = new System.Windows.Forms.Label();
+            this.lblcoin001 = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.textBoxvaluebuy = new System.Windows.Forms.TextBox();
+            this.textBoxpay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelbuy
+            // 
+            this.labelbuy.AutoSize = true;
+            this.labelbuy.Location = new System.Drawing.Point(15, 27);
+            this.labelbuy.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelbuy.Name = "labelbuy";
+            this.labelbuy.Size = new System.Drawing.Size(122, 17);
+            this.labelbuy.TabIndex = 0;
+            this.labelbuy.Text = "Valor da Compra:";
+            // 
+            // labelpaymoney
+            // 
+            this.labelpaymoney.AutoSize = true;
+            this.labelpaymoney.Location = new System.Drawing.Point(15, 76);
+            this.labelpaymoney.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelpaymoney.Name = "labelpaymoney";
+            this.labelpaymoney.Size = new System.Drawing.Size(164, 17);
+            this.labelpaymoney.TabIndex = 1;
+            this.labelpaymoney.Text = "Valor pago em dinheiro:";
+            // 
+            // labelvaluepay
+            // 
+            this.labelvaluepay.AutoSize = true;
+            this.labelvaluepay.Location = new System.Drawing.Point(15, 118);
+            this.labelvaluepay.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelvaluepay.Name = "labelvaluepay";
+            this.labelvaluepay.Size = new System.Drawing.Size(82, 17);
+            this.labelvaluepay.TabIndex = 2;
+            this.labelvaluepay.Text = "Valor pago:";
+            // 
+            // labelresultbuy
+            // 
+            this.labelresultbuy.AutoSize = true;
+            this.labelresultbuy.Location = new System.Drawing.Point(125, 118);
+            this.labelresultbuy.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelresultbuy.Name = "labelresultbuy";
+            this.labelresultbuy.Size = new System.Drawing.Size(16, 17);
+            this.labelresultbuy.TabIndex = 3;
+            this.labelresultbuy.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(345, 112);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Moedas para troco";
+            // 
+            // resultcoin1
+            // 
+            this.resultcoin1.AutoSize = true;
+            this.resultcoin1.Location = new System.Drawing.Point(345, 153);
+            this.resultcoin1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.resultcoin1.Name = "resultcoin1";
+            this.resultcoin1.Size = new System.Drawing.Size(16, 17);
+            this.resultcoin1.TabIndex = 5;
+            this.resultcoin1.Text = "0";
+            // 
+            // lblcoin1
+            // 
+            this.lblcoin1.AutoSize = true;
+            this.lblcoin1.Location = new System.Drawing.Point(381, 153);
+            this.lblcoin1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcoin1.Name = "lblcoin1";
+            this.lblcoin1.Size = new System.Drawing.Size(132, 17);
+            this.lblcoin1.TabIndex = 6;
+            this.lblcoin1.Text = "Moedas de R$1.00";
+            // 
+            // resultcoin05
+            // 
+            this.resultcoin05.AutoSize = true;
+            this.resultcoin05.Location = new System.Drawing.Point(345, 184);
+            this.resultcoin05.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.resultcoin05.Name = "resultcoin05";
+            this.resultcoin05.Size = new System.Drawing.Size(16, 17);
+            this.resultcoin05.TabIndex = 7;
+            this.resultcoin05.Text = "0";
+            // 
+            // lblcoin05
+            // 
+            this.lblcoin05.AutoSize = true;
+            this.lblcoin05.Location = new System.Drawing.Point(381, 184);
+            this.lblcoin05.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcoin05.Name = "lblcoin05";
+            this.lblcoin05.Size = new System.Drawing.Size(132, 17);
+            this.lblcoin05.TabIndex = 8;
+            this.lblcoin05.Text = "Moedas de R$0.50";
+            // 
+            // resultcoin025
+            // 
+            this.resultcoin025.AutoSize = true;
+            this.resultcoin025.Location = new System.Drawing.Point(345, 213);
+            this.resultcoin025.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.resultcoin025.Name = "resultcoin025";
+            this.resultcoin025.Size = new System.Drawing.Size(16, 17);
+            this.resultcoin025.TabIndex = 9;
+            this.resultcoin025.Text = "0";
+            // 
+            // lblcoin025
+            // 
+            this.lblcoin025.AutoSize = true;
+            this.lblcoin025.Location = new System.Drawing.Point(381, 213);
+            this.lblcoin025.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcoin025.Name = "lblcoin025";
+            this.lblcoin025.Size = new System.Drawing.Size(132, 17);
+            this.lblcoin025.TabIndex = 10;
+            this.lblcoin025.Text = "Moedas de R$0.25";
+            // 
+            // resultcoin010
+            // 
+            this.resultcoin010.AutoSize = true;
+            this.resultcoin010.Location = new System.Drawing.Point(345, 243);
+            this.resultcoin010.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.resultcoin010.Name = "resultcoin010";
+            this.resultcoin010.Size = new System.Drawing.Size(16, 17);
+            this.resultcoin010.TabIndex = 11;
+            this.resultcoin010.Text = "0";
+            // 
+            // lblcoin010
+            // 
+            this.lblcoin010.AutoSize = true;
+            this.lblcoin010.Location = new System.Drawing.Point(381, 243);
+            this.lblcoin010.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcoin010.Name = "lblcoin010";
+            this.lblcoin010.Size = new System.Drawing.Size(132, 17);
+            this.lblcoin010.TabIndex = 12;
+            this.lblcoin010.Text = "Moedas de R$0.10";
+            // 
+            // resultcoin005
+            // 
+            this.resultcoin005.AutoSize = true;
+            this.resultcoin005.Location = new System.Drawing.Point(345, 274);
+            this.resultcoin005.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.resultcoin005.Name = "resultcoin005";
+            this.resultcoin005.Size = new System.Drawing.Size(16, 17);
+            this.resultcoin005.TabIndex = 13;
+            this.resultcoin005.Text = "0";
+            // 
+            // lblcoin005
+            // 
+            this.lblcoin005.AutoSize = true;
+            this.lblcoin005.Location = new System.Drawing.Point(381, 274);
+            this.lblcoin005.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcoin005.Name = "lblcoin005";
+            this.lblcoin005.Size = new System.Drawing.Size(132, 17);
+            this.lblcoin005.TabIndex = 14;
+            this.lblcoin005.Text = "Moedas de R$0,05";
+            // 
+            // resultcoin001
+            // 
+            this.resultcoin001.AutoSize = true;
+            this.resultcoin001.Location = new System.Drawing.Point(345, 303);
+            this.resultcoin001.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.resultcoin001.Name = "resultcoin001";
+            this.resultcoin001.Size = new System.Drawing.Size(16, 17);
+            this.resultcoin001.TabIndex = 15;
+            this.resultcoin001.Text = "0";
+            // 
+            // lblcoin001
+            // 
+            this.lblcoin001.AutoSize = true;
+            this.lblcoin001.Location = new System.Drawing.Point(381, 303);
+            this.lblcoin001.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcoin001.Name = "lblcoin001";
+            this.lblcoin001.Size = new System.Drawing.Size(132, 17);
+            this.lblcoin001.TabIndex = 16;
+            this.lblcoin001.Text = "Moedas de R$0,01";
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btn.Location = new System.Drawing.Point(15, 153);
+            this.btn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(306, 167);
+            this.btn.TabIndex = 17;
+            this.btn.Text = "Calcular";
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // textBoxvaluebuy
+            // 
+            this.textBoxvaluebuy.Location = new System.Drawing.Point(149, 24);
+            this.textBoxvaluebuy.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxvaluebuy.Name = "textBoxvaluebuy";
+            this.textBoxvaluebuy.Size = new System.Drawing.Size(127, 25);
+            this.textBoxvaluebuy.TabIndex = 18;
+            this.textBoxvaluebuy.TextChanged += new System.EventHandler(this.textBoxvaluebuy_TextChanged);
+            // 
+            // textBoxpay
+            // 
+            this.textBoxpay.Location = new System.Drawing.Point(194, 67);
+            this.textBoxpay.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxpay.Name = "textBoxpay";
+            this.textBoxpay.Size = new System.Drawing.Size(127, 25);
+            this.textBoxpay.TabIndex = 19;
+            // 
+            // Telaprincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(534, 346);
+            this.Controls.Add(this.textBoxpay);
+            this.Controls.Add(this.textBoxvaluebuy);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.lblcoin001);
+            this.Controls.Add(this.resultcoin001);
+            this.Controls.Add(this.lblcoin005);
+            this.Controls.Add(this.resultcoin005);
+            this.Controls.Add(this.lblcoin010);
+            this.Controls.Add(this.resultcoin010);
+            this.Controls.Add(this.lblcoin025);
+            this.Controls.Add(this.resultcoin025);
+            this.Controls.Add(this.lblcoin05);
+            this.Controls.Add(this.resultcoin05);
+            this.Controls.Add(this.lblcoin1);
+            this.Controls.Add(this.resultcoin1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelresultbuy);
+            this.Controls.Add(this.labelvaluepay);
+            this.Controls.Add(this.labelpaymoney);
+            this.Controls.Add(this.labelbuy);
+            this.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Telaprincipal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calcular o Troco em moedas";
+            this.Load += new System.EventHandler(this.Telaprincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelbuy;
+        private System.Windows.Forms.Label labelpaymoney;
+        private System.Windows.Forms.Label labelvaluepay;
+        private System.Windows.Forms.Label labelresultbuy;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label resultcoin1;
+        private System.Windows.Forms.Label lblcoin1;
+        private System.Windows.Forms.Label resultcoin05;
+        private System.Windows.Forms.Label lblcoin05;
+        private System.Windows.Forms.Label resultcoin025;
+        private System.Windows.Forms.Label lblcoin025;
+        private System.Windows.Forms.Label resultcoin010;
+        private System.Windows.Forms.Label lblcoin010;
+        private System.Windows.Forms.Label resultcoin005;
+        private System.Windows.Forms.Label lblcoin005;
+        private System.Windows.Forms.Label resultcoin001;
+        private System.Windows.Forms.Label lblcoin001;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox textBoxvaluebuy;
+        private System.Windows.Forms.TextBox textBoxpay;
+    }
+}
---
+++ CalculoTrocodemoedas/View/Telaprincipal.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoTrocodemoedas.View
+{
+    public partial class Telaprincipal : Form
+    {
+        public Telaprincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Telaprincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+
+        //declaramos as variáveis para o tipo decimal
+
+
+        {
+            decimal valuebuy;
+
+            decimal valuepay;
+
+            decimal rest;
+
+            // receber os valores nas decimas delaradas e converter para o decimal
+
+            valuebuy = Convert.ToDecimal(textBoxvaluebuy.Text);
+            valuepay = Convert.ToDecimal(textBoxpay.Text);
+
+            if (valuebuy < valuepay)
+            {
+                
+
+                    rest = valuepay - valuebuy;
+
+                    labelresultbuy.Text = rest.ToString();
+
+                    int coin100 = (int)(rest / 1);
+                    rest = rest % 1m;
+                    resultcoin1.Text = coin100.ToString();
+
+                    //Resto = resto % 1;
+
+                    int coin050 = (int)(rest / 0.50m);
+                    rest = rest % 0.50m;
+                    resultcoin05.Text = coin050.ToString();
+
+                    int coin025 = (int)(rest / 0.25m);
+                    rest = rest % 0.25m;
+                    resultcoin025.Text = coin025.ToString();
+
+                    int coin010 = (int)(rest / 0.10m);
+                    rest = rest % 0.10m;
+                    resultcoin010.Text = coin010.ToString();
+
+                    int coin005 = (int)(rest / 0.05m);
+                    rest = rest % 0.05m;
+                    resultcoin005.Text = coin005.ToString();
+
+                    int coin001 = (int)(rest / 0.01m);
+                    rest = rest % 0.01m;
+                    resultcoin001.Text = coin001.ToString();
+
+
+
+
+
+
+                
+            }else
+            {
+                MessageBox.Show("Falta Dinheiro");
+            }
+
+
+             if (textBoxvaluebuy.SelectedText == "" || textBoxpay.SelectedText == "")
+             {
+                    MessageBox.Show("Digite os Valores");
+             } else
+             { }
+
+            }
+      
+        private void textBoxvaluebuy_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
+
+
+
+ 
+
+
